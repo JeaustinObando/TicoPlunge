c0ff8d440037833bf2fb04c71ca43dfa5b2c1794
Isaac <isaacgatgens13@gmail.com>
2024-04-27 11:55:32 -0600

    separando los html para mostrar diferente segun quien este logeado

--- Frontend/src/GlobalVariables.js
+++ Frontend/src/GlobalVariables.js
@@ -2,10 +2,6 @@ import axios from "axios";
 
 export const baseUrl = "http://localhost:8080";
 
-// export const urlFeedback = `${baseUrl}/Feedback`;
-// export const urlAppointment = `${baseUrl}/Appointment`;
-// export const urlClass = `${baseUrl}/Class`;
-
 export const urlFeedback = `${baseUrl}/comentarios`;
 export const urlAppointment = `${baseUrl}/comentarios`;
 export const urlClass = `${baseUrl}/comentarios`;
