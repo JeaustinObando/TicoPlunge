044acb91efcb8b7edb2dc25e785f9ddb8a4abc1d
Emmanuel Lopez Ramirez <emma_1399@estudiantec.cr>
2024-05-03 23:17:33 -0600

    New Backend

---
+++ Backend/Routes/users.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const { User, validate } = require("../Models/users");
+const bcrypt = require("bcrypt");
+
+router.post("/", async (req, res) => {
+	try {
+        console.log(req.body);
+
+		const { error } = validate(req.body);
+
+        /*if (error)
+			return res.status(400).send({ message: error.details[0].message });*/
+
+		const user = await User.findOne({ email: req.body.email });
+		if (user)
+			return res
+				.status(409)
+				.send({ message: "User with given email already Exist!" });
+
+		const salt = await bcrypt.genSalt(Number(process.env.SALT));
+        console.log(`Este es salt: ${salt}`)
+		const hashPassword = await bcrypt.hash(req.body.password, salt);
+        console.log(`Este es hashpasword: ${hashPassword}`)
+		await new User({ ...req.body, password: hashPassword }).save();
+		console.log(error)
+		res.status(201).send({ message: "User created successfully" });
+	} catch (error) {
+		res.status(500).send({ message: "Internal Server Error" });
+	}
+});
+
+module.exports = router;
